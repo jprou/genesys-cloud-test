a06b6003b8cdbdce5e42008add74e6bdcab76079
Jean-Pierre <54180799+JeanPierreRousseau@users.noreply.github.com>
2023-06-12 16:05:44 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import { ref, onMounted } from 'vue'
 onMounted(() => {
   console.log("Crossssss");
     var client = platformClient.ApiClient.instance;
+    client.environment = "mypurecloud.ie";
     console.log("Crossclient" + JSON.stringify(client));
     var clientId = "6ea9ac07-8f3f-4444-8800-f5cb40b13a41";
     var redirectUri = "https://jp-cloud-test.onrender.com/";
