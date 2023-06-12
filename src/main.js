3c23a9cf962b9fdfd9ef80e58b75c2e84d24c00e
Jean-Pierre <54180799+JeanPierreRousseau@users.noreply.github.com>
2023-06-12 12:26:16 +0200

    bug fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import {platformClient} from 'purecloud-client-app-sdk';
 import App from './App.vue'
 
 createApp(App).mount('#app')
@@ -8,7 +9,7 @@ createApp(App).mount('#app')
     let urlParams = new URLSearchParams(window.location.search);
     alert(`Your interaction id is: ${urlParams.get('id')}`);
 
-    var platformClient = require('platformClient');
+    // var platformClient = require('purecloud-client-app-sdk');
     var client = platformClient.ApiClient.instance;
     var clientId = "6ea9ac07-8f3f-4444-8800-f5cb40b13a41";
     var redirectUri = "https://jp-cloud-test.onrender.com/";
